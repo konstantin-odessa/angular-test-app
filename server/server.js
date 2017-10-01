ae9a5a91d52c1d59bb3d7ec9dd08cf74fc7fc1c4
Smith John <Smith John>
2017-10-02 01:37:04 +0300

    server

--- server/server.js
+++ server/server.js
@@ -1,17 +1,29 @@
+'use strict';
+
 const express = require('express');
-const bodyParser= require('body-parser');
+const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
+const jwt = require('express-jwt');
+const cors = require('cors');
 
 const app = express();
 
 let db;
+const MONGO_DB_AUTH_URL = 'mongodb://konstantin_odessa:IGNATIA@ds151014.mlab.com:51014/mongo-db-test';
+const AUTH0_SECRET = 'ogCpx0wwmymrJO812syBWhrY4SHSYJMGfzePdYUoYxnvsFB4-qyritLVTW3iH2eb';
+
+const authCheck = jwt({
+    secret: new Buffer(AUTH0_SECRET, 'base64'),
+    audience: 'https://konstantin-odessa.eu.auth0.com/userinfo',
+});
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(cors());
 
 app.post('/markers', (req, res) => {
     db.collection('markers').insertMany(req.body)
-        .then(function(result) {
+        .then(function (result) {
             console.log(result);
             console.log('saved to database');
             res.send({success: true});
@@ -27,15 +39,15 @@ app.post('/markers', (req, res) => {
     // })
 
 });
-app.get('/all', (req, res) => {
-    db.collection('markers').find({}).toArray(function(err, results) {
+app.get('/all', authCheck, (req, res) => {
+    db.collection('markers').find({}).toArray(function (err, results) {
         if (err) return console.log(err);
         console.log(results);
         res.send(results);
     });
 });
 
-MongoClient.connect('mongodb://konstantin_odessa:IGNATIA@ds151014.mlab.com:51014/mongo-db-test', (err, database) => {
+MongoClient.connect(MONGO_DB_AUTH_URL, (err, database) => {
     if (err) return console.log(err);
     db = database;
     app.listen(3000, () => {
